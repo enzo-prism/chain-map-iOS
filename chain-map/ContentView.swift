f2d53b8e070091e50eccd4152e6d9c0a4050850c
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2026-01-10 19:30:44 -0800

    Initial Commit

---
+++ chain-map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chain-map
+//
+//  Created by Enzo on 1/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chain-map/chain_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  chain_mapApp.swift
+//  chain-map
+//
+//  Created by Enzo on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct chain_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
